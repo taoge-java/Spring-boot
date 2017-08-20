dff030a2267b16fdf44586702a28e50e89a73014
zengjintao <Administrator@SC-201706212248>
2017-08-20 21:11:18 +0800

    .

--- src/main/java/com/springboot/interceptor/WebAppConfig.java
+++ src/main/java/com/springboot/interceptor/WebAppConfig.java
@@ -2,12 +2,20 @@ package com.springboot.interceptor;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 @Component
 public class WebAppConfig extends WebMvcConfigurerAdapter{
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new ViewInterceptor());
+		//registry.addInterceptor(new ViewInterceptor());
+	}
+	
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		 //配置静态资源路径
+	     registry.addResourceHandler("/resource/**").addResourceLocations("classpath:/resource/");
 	}
 }
